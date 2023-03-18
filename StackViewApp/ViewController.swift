62f0638ecda9d824d2862903e97acf0890a3a271
Danila Kornev <danilakornev92@gmail.com>
2023-03-19 00:25:07 +0300

    Initial Commit

---
+++ StackViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewApp
+//
+//  Created by Danila Kornev on 19.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
